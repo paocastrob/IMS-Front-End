23443088e0f2e0029182fbe55f3f0c324ce294dc
PaolaC <paocastrob@gmail.com>
2019-08-16 14:33:31 +0000

    mapping inventory

--- src/js/views/demo.js
+++ src/js/views/demo.js
@@ -8,41 +8,49 @@ import "../../styles/demo.scss";
 export class Demo extends React.Component {
 	render() {
 		return (
-			<div className="container">
-				<ul className="list-group">
-					<h1>adsfasdf</h1>
-					<Context.Consumer>
-						{({ store, actions }) => {
-							return store.purchases.map((item, index) => {
-								return (
-									<li
-										key={index}
-										className="list-group-item d-flex justify-content-between"
-										style={{ background: item.background }}>
-										<Link to={"/single/" + index}>
-											<p>Link to: {item.title} </p>
-											<p>SKU #: {item.sku} </p>
-											<p>Description #: {item.description} </p>
-											<p>QTY #: {item.quantity} </p>
-										</Link>
-										<p style={{ color: item.initial }}>
-											{"Check store/store.js scroll to the actions to see the code "}
-										</p>
-										<button
-											className="btn btn-success"
-											onClick={() => actions.changeColor(index, "orange")}>
-											Change Color
-										</button>
-									</li>
-								);
-							});
-						}}
-					</Context.Consumer>
-				</ul>
-				<br />
-				<Link to="/">
-					<button className="btn btn-primary">Back home</button>
-				</Link>
+			<div className="row">
+				<div className="col-6 mx-auto">
+					<div className="text-center mt-5">
+						<h1>Purchases</h1>
+					</div>
+					<div>
+						<p> August 15, 2019 </p> <p>0123456789 (order number)</p>
+					</div>
+					<table className="table table-bordered">
+						<thead>
+							<tr>
+								<th scope="col">ID Number</th>
+								<th scope="col">SKU</th>
+								<th scope="col">Description</th>
+								<th scope="col">Quantity</th>
+								<th scope="col">Date</th>
+								<th scope="col">Order Number</th>
+								<th scope="col">{/*space for x's*/}</th>
+							</tr>
+						</thead>
+						<tbody>
+							<Context.Consumer>
+								{({ store, actions }) => {
+									return store.purchases.map((item, index) => {
+										return (
+											<tr key={index}>
+												<th scope="row">{index + 1}</th>
+												<td>{item.title}</td>
+												<td>{item.sku}</td>
+												<td>{item.description}</td>
+												<td>@mdo</td>
+												<td>@mdo</td>
+												<td>
+													<i className="far fa-times-circle fa-2x pt-2 pl-1 text-danger" />
+												</td>
+											</tr>
+										);
+									});
+								}}
+							</Context.Consumer>
+						</tbody>
+					</table>
+				</div>
 			</div>
 		);
 	}
