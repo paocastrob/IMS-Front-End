0fdbbe3160a96b25956639a91b0c4d5c2acafc9f
JpHendrix51 <jonathan.e.perez@gmail.com>
2019-08-21 14:40:36 +0000

    added table to inventory

--- src/js/views/inventory.js
+++ src/js/views/inventory.js
@@ -5,45 +5,67 @@ import { Context } from "../store/appContext";
 export class Inventory extends React.Component {
 	render() {
 		return (
-			<div className="text-center mt-5">
-				<div className="col-4 offset-4">
-					<div className="text-center mt-5">
-						<h1>Homepage</h1>
-					</div>
-					<table className="table table-bordered">
-						<thead>
+			<form>
+				<div className="form-group">
+					<label htmlFor="exampleInputEmail1">Email address</label>
+					<input
+						type="email"
+						className="form-control"
+						id="exampleInputEmail1"
+						aria-describedby="emailHelp"
+						placeholder="Enter email"
+					/>
+					<small id="emailHelp" className="form-text text-muted">
+						Well never share your email with anyone else.
+					</small>
+				</div>
+				<div className="form-group">
+					<label htmlFor="exampleInputPassword1">Password</label>
+					<input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+				</div>
+				<div className="form-check">
+					<input type="checkbox" className="form-check-input" id="exampleCheck1" />
+					<label className="form-check-label" htmlFor="exampleCheck1">
+						Check me out
+					</label>
+				</div>
+				<button type="submit" className="btn btn-primary">
+					Submit
+				</button>
+				<div className="text-center mt-5">
+					<div className="col-4 offset-4">
+						<table className="table table-bordered">
+							<thead>
+								<Context.Consumer>
+									{({ store, actions }) => {
+										return (
+											store.products[0] &&
+											Object.keys(store.products[0]).map(key => {
+												return <td key={key}>{key}</td>;
+											})
+										);
+									}}
+								</Context.Consumer>
+							</thead>
+							​
 							<Context.Consumer>
 								{({ store, actions }) => {
-									return (
-										store.products[0] &&
-										Object.keys(store.products[0]).map(key => {
-											return (
-												<td key={key} style={{ fontWeight: "bold" }}>
-													{key}
-												</td>
-											);
-										})
-									);
+									return store.products.map((item, index) => {
+										return (
+											<tr key={index}>
+												<td>{item.description}</td>
+												<td>{item.id}</td>
+												<td>{item.item}</td>
+												<td>{item.qty}</td>
+											</tr>
+										);
+									});
 								}}
 							</Context.Consumer>
-						</thead>
-						<Context.Consumer>
-							{({ store, actions }) => {
-								return store.products.map((item, index) => {
-									return (
-										<tr key={index}>
-											<td>{item.description}</td>
-											<td>{item.id}</td>
-											<td>{item.item}</td>
-											<td>{item.qty}</td>
-										</tr>
-									);
-								});
-							}}
-						</Context.Consumer>
-					</table>
+						</table>
+					</div>
 				</div>
-			</div>
+			</form>
 		);
 	}
 }
