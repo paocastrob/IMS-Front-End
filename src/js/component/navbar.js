24450f2afa921e3f2b985b2c289cb94dac75b6f5
PaolaC <paocastrob@gmail.com>
2019-08-21 20:29:44 +0000

    Black Footer and Navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export class Navbar extends React.Component {
 	render() {
 		return (
-			<nav className="navbar navbar-expand-lg navbar-light bg-light">
+			<nav className="navbar navbar-expand-lg navbar-dark thedarkness">
 				<button
 					className="navbar-toggler"
 					type="button"
