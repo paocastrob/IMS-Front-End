e0a298ce31765f9017edacbc3191bc58e72c69bc
JpHendrix51 <jonathan.e.perez@gmail.com>
2019-08-28 19:49:04 +0000

    added dashboard

--- src/js/views/dashboard.js
+++ src/js/views/dashboard.js
@@ -21,27 +21,53 @@ export class Dashboard extends React.Component {
 				<div className="fila row">
 					<div className="col-8 offset-2 mt-5">
 						<div className="row">
-							<div className="column col-6 bg-primary border border-dark text-center text-light">
-								<i className="fas fa-cart-arrow-down fa-5x mt-5" />
+							<div className="inventory column col-6 bg-primary border border-dark text-center text-light">
+								<i className="inventory icon fas fa-cart-arrow-down fa-5x mt-5" />
+								<div className="overlay3">
+									<div className="text3">
+										<h2>Purchases</h2>
+									</div>
+								</div>
 							</div>
-							<div className="column col-6 bg-primary border border-dark text-light text-center">
-								<i className="fas fa-dolly-flatbed fa-5x mt-5" />
+							<div className="sales column col-6 bg-primary border border-dark text-light text-center">
+								<i className="salesicon fas fa-dolly-flatbed fa-5x mt-5" />
+								<div className="overlay2">
+									<div className="text2">
+										<h2>Sales</h2>
+									</div>
+								</div>
 							</div>
 						</div>
 						<div className="row">
-							<div className="col column bg-info border border-dark text-light text-center">
-								<i className="fas fa-boxes fa-7x mt-4" />
+							<div className="purchases col column bg-info border border-dark text-light text-center">
+								<i className="purchasesicon fas fa-boxes fa-7x mt-4" />
+								<div className="overlay">
+									<div className="text">
+										<h2>Inventory</h2>
+									</div>
+								</div>
 							</div>
 						</div>
 						<div className="row">
-							<div className="column col-2 bg-primary border border-dark text-light text-center">
-								<i className="fas fa-list fa-4x mt-5" />
+							<div className="list column col-2 bg-primary border border-dark text-light text-center">
+								<i className="listicon fas fa-list fa-4x mt-5" />
+								<div className="overlay5">
+									<div className="text5">Sales List</div>
+								</div>
 							</div>
-							<div className="column col-2 bg-info border border-dark text-light text-center">
-								<i className="fas fa-list-ul fa-4x mt-5" />
+							<div className="list column col-2 bg-info border border-dark text-light text-center">
+								<i className="listicon fas fa-list-ul fa-4x mt-5" />
+								<div className="overlay5">
+									<div className="text5">Purchase List</div>
+								</div>
 							</div>
-							<div className="column col-8 logistics border border-dark text-light text-center">
-								<i className="fas fa-motorcycle fa-5x mt-5" />
+							<div className="delivery column col-8 logistics border border-dark text-light text-center">
+								<i className="deliveryicon fas fa-motorcycle fa-5x mt-5" />
+								<div className="overlay4">
+									<div className="text4">
+										<h2>Delivery</h2>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
