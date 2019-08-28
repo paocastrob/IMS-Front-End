23db5a8b39d2365f2ba3bf3217bf12a8725f8a30
JpHendrix51 <jonathan.e.perez@gmail.com>
2019-08-28 21:06:41 +0000

    added dashboard

--- src/js/views/dashboard.js
+++ src/js/views/dashboard.js
@@ -2,6 +2,7 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
 export class Dashboard extends React.Component {
 	render() {
 		return (
@@ -18,56 +19,69 @@ export class Dashboard extends React.Component {
 					<li />
 					<li />
 				</ul>
-				<div className="fila row">
+				<div className="fila row mb-5">
 					<div className="col-8 offset-2 mt-5">
 						<div className="row">
-							<div className="inventory column col-6 bg-primary border border-dark text-center text-light">
-								<i className="inventory icon fas fa-cart-arrow-down fa-5x mt-5" />
-								<div className="overlay3">
-									<div className="text3">
-										<h2>Purchases</h2>
+							<div className="inventory column col border border-dark text-center text-light">
+								<Link to="/private/purchases">
+									<i className="inventory icon fas fa-cart-arrow-down fa-5x mt-5" />
+									<div className="overlay3">
+										<div className="text3">
+											<h2>Purchases</h2>
+										</div>
 									</div>
-								</div>
+								</Link>
 							</div>
-							<div className="sales column col-6 bg-primary border border-dark text-light text-center">
-								<i className="salesicon fas fa-dolly-flatbed fa-5x mt-5" />
-								<div className="overlay2">
-									<div className="text2">
-										<h2>Sales</h2>
+							<div className="sales column ml-0 col bg-primary border border-dark text-light text-center">
+								<Link to="/private/sales">
+									<i className="salesicon fas fa-dolly-flatbed fa-5x mt-5" />
+									<div className="overlay2">
+										<div className="text2">
+											<h2>Sales</h2>
+										</div>
 									</div>
-								</div>
+								</Link>
 							</div>
 						</div>
 						<div className="row">
-							<div className="purchases col column bg-info border border-dark text-light text-center">
-								<i className="purchasesicon fas fa-boxes fa-7x mt-4" />
-								<div className="overlay">
-									<div className="text">
-										<h2>Inventory</h2>
+							<div className="purchases col column bg-info border-dark text-light text-center">
+								<Link to="/private/inventory">
+									<i className="purchasesicon fas fa-boxes fa-7x mt-4" />
+									<div className="overlay">
+										<div className="text">
+											<h2>Inventory</h2>
+										</div>
 									</div>
-								</div>
+								</Link>
 							</div>
 						</div>
+
 						<div className="row">
-							<div className="list column col-2 bg-primary border border-dark text-light text-center">
-								<i className="listicon fas fa-list fa-4x mt-5" />
-								<div className="overlay5">
-									<div className="text5">Sales List</div>
-								</div>
+							<div className="list column col bg-primary border border-dark text-light text-center">
+								<Link to="/private/">
+									<i className="listicon fas fa-list fa-4x mt-5" />
+									<div className="overlay5">
+										<div className="text5">Sales List</div>
+									</div>
+								</Link>
 							</div>
-							<div className="list column col-2 bg-info border border-dark text-light text-center">
-								<i className="listicon fas fa-list-ul fa-4x mt-5" />
-								<div className="overlay5">
-									<div className="text5">Purchase List</div>
-								</div>
+							<div className="list column col bg-info border border-dark text-light text-center">
+								<Link to="/private/">
+									<i className="listicon fas fa-list-ul fa-4x mt-5" />
+									<div className="overlay5">
+										<div className="text5">Purchase List</div>
+									</div>
+								</Link>
 							</div>
 							<div className="delivery column col-8 logistics border border-dark text-light text-center">
-								<i className="deliveryicon fas fa-motorcycle fa-5x mt-5" />
-								<div className="overlay4">
-									<div className="text4">
-										<h2>Delivery</h2>
+								<Link to="/private/delivery">
+									<i className="deliveryicon fas fa-motorcycle fa-5x mt-5" />
+									<div className="overlay4">
+										<div className="text4">
+											<h2>Delivery</h2>
+										</div>
 									</div>
-								</div>
+								</Link>
 							</div>
 						</div>
 					</div>
