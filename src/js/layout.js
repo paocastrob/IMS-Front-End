d2ff88690c3bde6dc621d36d6c7bacc1292444f4
ColbyWTaylor <colbyt.093@gmail.com>
2019-08-29 16:31:21 +0000

    register

--- src/js/layout.js
+++ src/js/layout.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
 import { Login } from "./component/login";
+import { Register } from "./component/register";
 import { Purchases } from "./views/purchases";
 import { Sales } from "./views/sales";
 import { Inventory } from "./views/inventory";
@@ -28,6 +29,7 @@ export class Layout extends React.Component {
 					<ScrollToTop>
 						<Switch>
 							<Route exact path="/" component={Login} />
+							<Route exact path="/register" component={Register} />
 							<Route path="/private/dashboard" component={Dashboard} />
 							<Route
 								path="/private"
